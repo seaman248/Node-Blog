5d68bfd69e4835b4d92c2a45fc048cb7f9cbb27a
seaman <semen.bko@ya.ru>
2015-01-08 16:52:37 +0300

    add dinamic post list

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,13 @@ router.get('/', function(req, res) {
   	jumbotron: true ,
   	pages: [{name:'Blog', url: '/'},
   		{name: 'Portfolio', url: '/portfolio'},
-  		{name: 'Contacts', url: '/contacts'}]
+  		{name: 'Contacts', url: '/contacts'}],
+  	postList: [
+  		{title: 'Lorem ipsum.', postPrev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, maxime!'},
+  		{title: 'Lorem ipsum.', postPrev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, maxime!'},
+  		{title: 'Lorem ipsum.', postPrev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, maxime!'},
+
+  	]
   });
 });
 
