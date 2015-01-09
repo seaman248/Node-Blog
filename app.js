dd61c8c965cbcfcb8d517686f9fbb9d40b117e8e
seaman <semen.bko@ya.ru>
2015-01-09 14:40:40 +0300

    add contacts view and route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var post = require('./routes/post');
 var portfolio = require('./routes/portfolio');
+var contacts = require('./routes/contacts');
 var app = express();
 
 // view engine setup
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/blog', post);
 app.use('/portfolio', portfolio);
+app.use('/contacts', contacts);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
