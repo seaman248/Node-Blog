c6dd79207a410846afbfc4f210f86b3c82a0cf65
seaman <semen.bko@ya.ru>
2015-01-09 12:54:15 +0300

    add portfolio view

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var post = require('./routes/post');
+var portfolio = require('./routes/portfolio');
 var app = express();
 
 // view engine setup
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/blog', post);
+app.use('/portfolio', portfolio);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
