a53ef32bbb8513a8c33aa0248efec4dc748dc3a0
seaman <semen.bko@ya.ru>
2015-01-10 03:09:32 +0300

    add dbmodels aggregator

--- db/index.js
+++ db/index.js
@@ -1,2 +1,7 @@
 var  mongoose = require('mongoose');
 var models = require('./models.js');
+var conf = require('../conf');
+
+mongoose.connect(conf.get('db_connect'));
+
+module.exports.dbmodels = models;
