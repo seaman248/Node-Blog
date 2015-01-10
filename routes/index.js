4e2ad657095491397ed9964fc1df33d28b550df6
seaman <semen.bko@ya.ru>
2015-01-10 17:32:23 +0300

    add waterfall method for getPost operations to find posts in database

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
-
-var postList = [
-  		{title: 'lorem ipsum.', postPrev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, maxime!', href:'/blog/1'},
-  		{title: 'Lorem ipsum.', postPrev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, maxime!', href:'/blog/2'},
-  		{title: 'Lorem ipsum.', postPrev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, maxime!', href:'/blog/3'},
-  	];
-
-// var postList = db.Post.find();
+var async = require('async');
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-  	jumbotron: true,
-  	postList: postList
-  });
-});
+
+async.waterfall([
+	function getPosts (cb){
+		db.Post.find(function(err, posts){
+			if(err) console.log(err);
+			cb(null, posts);
+		})
+	}], function mainRouter (err, postList){
+		router.get('/', function(req, res, next) {
+		  res.render('index', {
+		  	jumbotron: true,
+		  	postList: postList
+		  });
+		});
+	})
 
 module.exports = router;
