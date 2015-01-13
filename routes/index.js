4b7e2d88769d07600cd55dd54996cdf9dab147af
seaman <semen.bko@ya.ru>
2015-01-13 04:13:27 +0300

    pagination works!

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,29 @@ var async = require('async');
 
 
 router.get('/', function(req, res, next) {
+	var skip = parseInt(req.query.n) || 0;
 	async.parallel([
 		function getPosts (cb){
 			db.Post
 				.find()
 				.sort({date:1})
-				.skip(0)
+				.skip(skip)
 				.limit(10)
 				.exec(function(err, posts){
 						if(err) console.log(err);
 						cb(null, posts);
 			})
+		},function getNumberOfPost (cb){
+			db.Post.find().count(function(err, num){
+				cb(null, num);
+			})
 		}], function mainRouter (err, results){
 				var postList = results[0];
 				res.render('index', {
 				jumbotron: true,
-				postList: postList
+				postList: postList,
+				countPosts: results[1],
+				skip: skip
 			});
 	});
 });
