d13558f88c4bc4fe091167ab8e0c391557a47825
seaman <semen.bko@ya.ru>
2015-01-17 17:02:13 +0300

    comment

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,14 @@ var async = require('async');
 router.get('/', function(req, res, next) {
 	var skip = parseInt(req.query.n) || 0;
 
-
 	async.parallel([
+		// Get posts from database
 		function getPostList (cb){
 			db.Post.getPostList(1, 10, skip, function(err, posts){
 				if (err) cb(err)
 				cb(null, posts)
 			});
+		// Get total number of posts in database
 		},function countPost (cb){
 			db.Post.countPost(function(err, num){
 				if(err) cb(err);
@@ -23,7 +24,7 @@ router.get('/', function(req, res, next) {
 				var postList = results[0];
 				var countPosts = results[1];
 
-
+				// render
 				res.render('index', {
 					jumbotron: true,
 					postList: postList,
